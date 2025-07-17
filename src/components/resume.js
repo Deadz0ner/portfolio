cf4adaf03d69069577d7bd31751cb3c56e8125c9
Shivansh <shivanshsagar3@gmail.com>
2025-07-17 12:08:59 +0530

    res fix_try_1

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,28 +1,3 @@
-import "../styles/resume.css";
-import { useEffect } from "react";
-
-const ResumeViewer = () => {
-  useEffect(() => {
-    document.body.classList.add("resume-fullscreen");
-    return () => {
-      document.body.classList.remove("resume-fullscreen");
-    };
-  }, []);
-
-  return (
-    <div className="resume-container fullscreen">
-      <div className="resume-frame-wrapper">
-        <iframe
-          src={`${process.env.PUBLIC_URL}/res.pdf`}
-          title="Resume"
-          width="100%"
-          height="100%"
-          className="resume-iframe"
-          style={{ minHeight: "100vh", position: "relative", zIndex: 1 }}
-        />
-      </div>
-    </div>
-  );
-};
-
-export default ResumeViewer;
+export default function Resume() {
+  return <div style={{ padding: 50, color: "white" }}>Resume Page Works</div>;
+}
